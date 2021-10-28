b1f1677574c6f9ccd6226602532df6ba8f562fc3
Marino Pavković <89594526+pavkovim@users.noreply.github.com>
2021-10-28 16:45:01 +0200

    Add files via upload

---
+++ assets/js/main.js
@@ -0,0 +1,9 @@
+/*TOGGLE MENU START */   
+    var navLinks = document.getElementById("navLinks");
+    function showMenu() {
+        navLinks.style.right = "0";
+    }
+    function hideMenu(){
+        navLinks.style.right = "-200px";
+    }
+/*TOGGLE MENU END */   
